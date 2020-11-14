27d2278467b3dcb78781cb4b7edacb56adc11c5f
Biancaasan <bianca.asan009@yahoo.com>
2020-11-14 13:58:51 +0200

    Am adaugat membrul time la clasa Equipment!

--- tema/Equipment.java
+++ tema/Equipment.java
@@ -2,10 +2,16 @@ package tema;
 
 public class Equipment implements IEquipment {
     protected int delay;
+    protected int time;	
 
     public Equipment(int delay) {
         this.delay = delay;
     }
+
+    public Equipment(int time) {
+        this.dtime = time;
+    }
+
 
     public Equipment() {
     }
