d50dd18b4d73e1a21c42849b5bacca6ed7e4ee69
Biancaasan <bianca.asan009@yahoo.com>
2020-11-14 13:52:39 +0200

    Am implementat clasa PacketBinary :)

--- tema/PacketBinary.java
+++ tema/PacketBinary.java
@@ -1,4 +1,24 @@
 package tema;
 
 public class PacketBinary extends Packet{
+ int numberBytes;
+
+    public PacketBinary(int initialTime, int time, int ipDest, int ipSource, int numberBytes) {
+        super(initialTime, time, ipDest, ipSource);
+        this.numberBytes = numberBytes;
+    }
+
+    @Override
+    public String toString() {
+        return "PacketBinary" +
+                "numberBytes=" + numberBytes;
+    }
+
+    public int getNumberBytes() {
+        return numberBytes;
+    }
+
+    public void setNumberBytes(int numberBytes) {
+        this.numberBytes = numberBytes;
+    }
 }
