3316674520cdfee53d079127a49f8028a121088d
ana0009 <ana.ungureanu@mta.ro>
2020-11-14 14:19:02 +0200

    Am implementat clasa PacketText

--- tema/PacketText.java
+++ tema/PacketText.java
@@ -1,4 +1,14 @@
 package tema;
 
 public class PacketText extends Packet{
+
+    private int numberLetters;
+
+
+    public int getNumberLetters(){return numberLetters;}
+
+    public PacketText(int nLetters)
+    {
+      numberLetters=nLetters;
+    };
 }
