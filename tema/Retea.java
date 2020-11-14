737cb24cc7af6a355fee1cf1a8c4384c84dd6c68
ana0009 <ana.ungureanu@mta.ro>
2020-11-14 14:22:32 +0200

    Am adaugat un membru si un geter la clasa Retea

--- tema/Retea.java
+++ tema/Retea.java
@@ -3,8 +3,11 @@ import java.util.ArrayList;
 
 public class Retea {
 
-    void PrintInfo();
+    public void PrintInfo();
+    private String numeRetea;
     private ArrayList<Router> ListaRoutere;
     private ArrayList<Switch> ListaSwtich;
+
+    public String getNume(){return numeRetea;}
 
 }
